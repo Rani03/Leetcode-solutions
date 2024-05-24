759be0fb6fc16b994a48dc6b074ad7f6ddc3e09e
Miss_Singh <64664188+Rani03@users.noreply.github.com>
2024-05-25 01:23:16 +0530

    Time: 105 ms (81.57%), Space: 75.2 MB (59.10%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.js
@@ -2,7 +2,7 @@
  * @param {string} text1
  * @param {string} text2
  * @return {number}
- */
+ */ /* there is another similar question at bottom too which is not availabel on leetcode */
 // will take 2d array to store if the same subsequence has been encountered or not. 
 var longestCommonSubsequence = function(text1, text2) {
     let m= text1.length;
@@ -14,9 +14,9 @@ var longestCommonSubsequence = function(text1, text2) {
                 {
                     if(text1[i-1]==text2[j-1])
                         {
-                            dp[i][j]=dp[i-1][j-1]+1;
+                            dp[i][j]=dp[i-1][j-1]+1;// if the charcters matched in both the strings then add 1
                         }
-                    else
+                    else // if it doesn't then
                         {
                             dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]); //take the maximum out of both the combination. In one combination, we will remove an element from the first text, in second combination, we will remove element from the second text.
                         }
@@ -26,3 +26,32 @@ var longestCommonSubsequence = function(text1, text2) {
     return dp[m][n];// return the utmost bottom right;
     
 };
+
+// Longest Common Substring, here the common charcters should appear one by one instead of not only following the order but they should be together too as in both the strings.
+
+
+// var LongestCommonSubstr = function(text1, text2)
+// {
+        
+//         let m=text1.length;
+//         let n=text2.length;
+//          let dp=Array.from(Array(m+1),()=>Array(n+1).fill(0));// will take 2d array to store if the same charcter has been encountered or not. 
+//          let max = 0;
+//     // Iterate over each position in the matrix
+//     for (let i = 1; i <= text1.length; i++)
+//       for (let j = 1; j <= text2.length; j++)
+
+//         // If characters are equal
+//         if (text1[i - 1] == text2[j - 1]) {
+//           // Get the number from diagonally opposite
+//           // and add 1
+//           dp[i][j] = dp[i - 1][j - 1] + 1;
+
+//           max = Math.max(dp[i][j], max);
+//         }
+//     return max;
+ 
+// 		}
+
+
+
